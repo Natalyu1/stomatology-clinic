608b6102166454d38cdc99b4736e2276a785619e
Natalyu1 <bratchickova2010@yandex.ru>
2022-09-29 16:36:13 +0400

    мобильная версия

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+let isOpen = false
 function openMenu() {
-    const openMobile = document.getElementById(container-mobile)
+    const openMobile = document.getElementById("container-mobile")
+    if (isOpen){
+      openMobile.style.display = "none"
+      isOpen = false
+    } else {
+        openMobile.style.display = "flex"
+        isOpen = true
+    }
 
 
 }
